7ae9fc135d7ebd26fb5c3109006eba27d8922f19
Iamashwink <ashwinkrithik41@gmail.com>
2026-07-24 19:05:27 +0530

    jwt added

--- src/auth/auth.service.js
+++ src/auth/auth.service.js
@@ -1,6 +1,7 @@
 const bcrypt=require('bcrypt')
 const repository=require('./auth.repository')
-
+const jwt=require('jsonwebtoken')
+require('dotenv').config()
 const register=async (username,email,password)=>{
     const existingUser=await repository.findUserByEmail(email)
 
@@ -10,8 +11,14 @@ const register=async (username,email,password)=>{
     }
     const hashedPassword=await bcrypt.hash(password,10);
     const user=await repository.createUser(username,email,hashedPassword);
-
-    return user
+    const accessToken=jwt.sign({
+        "id":user.id,
+        "username":user.username
+        },
+        process.env.JWT_SECRET,
+        {expiresIn:'10m'}
+    )
+    return accessToken
 }
 const login=async (email,password)=>{
     const user=await repository.findUserByEmail(email)
@@ -24,8 +31,16 @@ const login=async (email,password)=>{
     const isPassword= await bcrypt.compare(password,user.password)
     if(!isPassword)
         throw new Error('Invalid Credentials')
+    const accessToken=jwt.sign({
+        "id":user.id,
+        "username":user.username
+        },
+        process.env.JWT_SECRET,
+        {expiresIn:'10m'}
+    )
 
-    return 'Login Success'
+
+    return accessToken
 }
 
 const getMe=async (id) =>{
